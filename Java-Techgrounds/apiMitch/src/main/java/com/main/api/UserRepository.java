d8955ba0c8fe0dfab69d393de3f44f6e03905546
Mitch <mitchell.n@outlook.com>
2021-08-06 11:44:04 +0200

    Attempt at heroku

--- Java-Techgrounds/apiMitch/src/main/java/com/main/api/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.main.api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Student, Long> {
-
-    String toegevoegd = "";
-}
--- Java-Techgrounds/apiMitch/src/main/java/com/main/api/firebase/FirebaseInit.java
+++ Java-Techgrounds/apiMitch/src/main/java/com/main/api/firebase/FirebaseInit.java
@@ -1,29 +1,28 @@
 package com.main.api.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 
-@Service
+@Configuration
 public class FirebaseInit {
 
-    @PostConstruct
-    public void initialize() {
-        try {
-            FileInputStream serviceAccount = new FileInputStream("./ServiceAccount.json");
+    @Bean
+    public Firestore initialize() throws Exception {
+            FileInputStream serviceAccount = new FileInputStream("./firebase-account-info.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://portfolio-ace37.firebaseio.com")
                     .build();
 
-            FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+           FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
+           return FirestoreClient.getFirestore(firebaseApp);
     }
 }
